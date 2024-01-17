5cc29cb846f3a891b8c5a7c6eca71368bce50207
Ruben <111981761+R7b3nv0k@users.noreply.github.com>
2024-01-17 14:48:29 +0100

    Update feladat.py

--- feladat.py
+++ feladat.py
@@ -1,9 +1,10 @@
 import streamlit as st 
 
-st .set_page_config(
-  page_title="Multipage",
-  page_icon="",
-)
-st.title("Main Page")
-st.sidebar.sucsess("Select")
+st.set_page_config(
+  page_title="Multipage app",
+  page_icon="✔",
 
+)
+
+st.title("Main page")
+st.sidebar.successs("Válassz.")
